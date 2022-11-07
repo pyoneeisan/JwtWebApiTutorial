0648df932cedc4ca47d783040d408850ddf56b7e
pyone <pyoneeisan@gmail.com>
2022-11-07 16:12:13 +0700

    Get User Claims (with httpContextAccessor)

---
+++ JwtWebApiTutorial/Services/UserService/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+
+namespace JwtWebApiTutorial.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor1)
+        {
+            _httpContextAccessor = httpContextAccessor1;
+        }
+        public string GetMyName()
+        {
+            var result = string.Empty;
+            if(_httpContextAccessor.HttpContext!=null)
+            {
+                result = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            }
+            return result;
+        }
+    }
+}
